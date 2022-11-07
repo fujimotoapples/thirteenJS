5f5609771e26867c4531089d72f026797ae3b279
Kobe Fujimoto <fujikobe99@berkeley.edu>
2022-11-07 12:59:28 -0800

    initial commit

---
+++ program.js
@@ -0,0 +1,390 @@
+console.log('This is thirteen, the card game')
+//THREES
+const s3  = {
+	name : 'three of spades',
+	image: 'cards/3_of_spades.png',
+	value:1,
+	suit:'spades'
+}
+const c3 = {
+	name : 'three of clubs',
+	image: 'cards/3_of_clubs.png',
+	value:1,
+	suit:'clubs'
+}
+const d3 = {
+	name : 'three of diamonds',
+	image: 'cards/3_of_diamonds.png',
+	value:1,
+	suit:'diamonds'
+}
+const h3 = {
+	name : 'three of hearts',
+	image:'cards/3_of_hearts.png',
+	value:1,
+	suit:'hearts'
+}
+//FOURS
+const s4 = {
+	name : 'four of spades',
+	image: 'cards/4_of_spades.png',
+	value:2,
+	suit:'spades'
+}
+const c4 = {
+	name : 'four of clubs',
+	image: 'cards/4_of_clubs.png',
+	value:2,
+	suit:'clubs'
+}
+const d4 = {
+	name : 'four of diamonds',
+	image: 'cards/4_of_diamonds.png',
+	value:2,
+	suit:'diamonds'
+}
+const h4 = {
+	name : 'four of hearts',
+	image: 'cards/4_of_hearts.png',
+	value:2,
+	suit:'hearts'
+}
+//FIVES
+const s5 = {
+	name : 'five of spades',
+	image: 'cards/5_of_spades.png',
+	value:3,
+	suit:'spades'
+}
+const c5 = {
+	name : 'five of clubs',
+	image: 'cards/5_of_clubs.png',
+	value:3,
+	suit:'clubs'
+}
+const d5 = {
+	name : 'five of diamonds',
+	image: 'cards/5_of_diamonds.png',
+	value:3,
+	suit:'diamonds'
+}
+const h5 = {
+	name : 'five of hearts',
+	image: 'cards/5_of_hearts.png',
+	value:3,
+	suit:'hearts',
+}
+//SIX
+const s6 = {
+	name : 'six of spades',
+	image: 'cards/6_of_spades.png',
+	value:4,
+	suit:'spades',
+}
+const c6 = {
+	name : 'six of clubs',
+	image: 'cards/6_of_clubs.png',
+	value:4,
+	suit:'clubs',
+}
+const d6 = {
+	name : 'six of diamonds',
+	image: 'cards/6_of_diamonds.png',
+	value:4,
+	suit:'diamonds',
+}
+const h6 = {
+	name : 'six of hearts',
+	image: 'cards/6_of_hearts.png',
+	value:4,
+	suit:'hearts',
+}
+//SEVEN
+const s7 = {
+	name : 'seven of spades',
+	image: 'cards/7_of_spades.png',
+	value:5,
+	suit:'spades',
+}
+const c7 = {
+	name : 'seven of clubs',
+	image: 'cards/7_of_clubs.png',
+	value:5,
+	suit:'clubs',
+}
+const d7 = {
+	name : 'seven of diamonds',
+	image: 'cards/7_of_diamonds.png',
+	value:5,
+	suit:'diamonds',
+}
+const h7 = {
+	name : 'seven of hearts',
+	image: 'cards/7_of_hearts.png',
+	value:5,
+	suit:'hearts',
+}
+//EIGHT
+const s8 = {
+	name : 'eight of spades',
+	image: 'cards/8_of_spades.png',
+	value:6,
+	suit:'spades',
+}
+const c8 = {
+	name : 'eight of clubs',
+	image: 'cards/8_of_clubs.png',
+	value:6,
+	suit:'clubs',
+}
+const d8 = {
+	name : 'eight of diamonds',
+	image: 'cards/8_of_diamonds.png',
+	value:6,
+	suit:'diamonds',
+}
+const h8 = {
+	name : 'eight of hearts',
+	image: 'cards/8_of_hearts.png',
+	value:6,
+	suit:'hearts',
+}
+//NINE
+const s9 = {
+	name : 'nine of spades',
+	image: 'cards/9_of_spades.png',
+	value:7,
+	suit:'spades',
+}
+const c9 = {
+	name : 'nine of clubs',
+	image: 'cards/9_of_clubs.png',
+	value:7,
+	suit:'clubs',
+}
+const d9 = {
+	name : 'nine of diamonds',
+	image: 'cards/9_of_diamonds.png',
+	value:7,
+	suit:'diamonds',
+}
+const h9 = {
+	name : 'nine of hearts',
+	image: 'cards/9_of_hearts.png',
+	value:7,
+	suit:'hearts',
+}
+//TENS
+const s10 = {
+	name : 'ten of spades',
+	image: 'cards/10_of_spades.png',
+	value:8,
+	suit:'spades',
+}
+const c10 = {
+	name : 'ten of clubs',
+	image: 'cards/10_of_clubs.png',
+	value:8,
+	suit:'clubs',
+}
+const d10= {
+	name : 'ten of diamonds',
+	image:'cards/10_of_diamonds.png',
+	value:8,
+	suit:'diamonds',
+}
+const h10 = {
+	name : 'ten of hearts',
+	image: 'cards/10_of_hearts.png',
+	value:8,
+	suit:'hearts',
+}
+//JACKS
+const sj = {
+	name : 'jack of spades',
+	image: 'cards/jack_of_spades.png',
+	value:9,
+	suit:'spades',
+}
+const cj = {
+	name : 'jack of clubs',
+	image: 'cards/jack_of_clubs.png',
+	value:9,
+	suit:'clubs',
+}
+const dj = {
+	name : 'jack of diamonds',
+	image: 'cards/jack_of_diamonds.png',
+	value:9,
+	suit:'diamonds',
+}
+const hj = {
+	name : 'jack of hearts',
+	image:'cards/jack_of_hearts.png',
+	value:9,
+	suit:'hearts',
+}
+//QUEENS
+const sq = {
+	name : 'queen of spades',
+	image: 'cards/queen_of_spades.png',
+	value:10,
+	suit:'spades',
+}
+const cq = {
+	name : 'queen of clubs',
+	image: 'cards/queen_of_clubs.png',
+	value:10,
+	suit:'clubs',
+}
+const dq = {
+	name : 'queen of diamonds',
+	image: 'cards/queen_of_diamonds.png',
+	value:10,
+	suit:'diamonds',
+}
+const hq = {
+	name : 'queen of hearts',
+	image: 'cards/queen_of_hearts.png',
+	value:10,
+	suit:'hearts',
+}
+//KINGS
+const sk = {
+	name : 'king of spades',
+	image: 'cards/king_of_spades.png',
+	value:11,
+	suit:'spades',
+}
+const ck = {
+	name : 'king of clubs',
+	image: 'cards/king_of_clubs.png',
+	value:11,
+	suit:'clubs',
+}
+const dk = {
+	name : 'king of diamonds',
+	image: 'cards/king_of_diamonds.png',
+	value:11,
+	suit:'diamonds',
+}
+const hk = {
+	name : 'king of hearts',
+	image: 'cards/king_of_hearts.png',
+	value:11,
+	suit:'hearts',
+}
+//ACES
+const sa = {
+	name : 'ace of spades',
+	image: 'cards/ace_of_spades.png',
+	value:12,
+	suit:'spades',
+}
+const ca = {
+	name : 'ace of clubs',
+	image: 'cards/ace_of_clubs.png',
+	value:12,
+	suit:'clubs',
+}
+const da = {
+	name : 'ace of diamonds',
+	image: 'cards/ace_of_diamonds.png',
+	value:12,
+	suit:'diamonds',
+}
+const ha = {
+	name : 'ace of hearts',
+	image: 'cards/ace_of_hearts.png',
+	value:12,
+	suit:'hearts',
+}
+//TWOS
+const s2 = {
+	name : 'two of spades',
+	image: 'cards/2_of_spades.png',
+	value:13,
+	suit:'spades',
+}
+const c2 = {
+	name : 'two of clubs',
+	image:'cards/2_of_clubs.png',
+	value:13,
+	suit:'clubs',
+}
+const d2 = {
+	name : 'two of diamonds',
+	image:'cards/2_of_diamonds.png' ,
+	value:13,
+	suit:'diamonds',
+}
+const h2 = {
+	name : 'two of hearts',
+	image: 'cards/2_of_hearts.png',
+	value: 13,
+	suit:'hearts',
+}
+
+
+//fisher-yates shuffle
+function shuffle(array) {
+  let currentIndex = array.length,randomIndex;
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+function deal(array){
+	shuffle(array);
+	let handOne = array.slice(0,13)
+	let handTwo= array.slice(14,27)
+	let handThree=array.slice(27,40)
+	let handFour=array.slice(40,53)
+	return [handOne,handTwo,handThree,handFour]
+}
+
+function pickHand(array,int){
+	let hand = array[int]
+	return hand
+}
+
+function displayHand(array){
+	for (item in array){
+		console.log(usersHandDisplaySpots[item])
+		console.log(array[item].image)
+		usersHandDisplaySpots[item].src=array[item].image
+	}
+}
+function getById(stringid){
+	return document.getElementById(stringid)
+}
+let deck = [s3,s4,s5,s6,s6,s7,s8,s9,s10,sj,sq,sk,sa,s2,c3,c4,c5,c6,c7,c8,c9,c10,cj,cq,ck,ca,c2,d3,d4,d5,d6,d7,d8,d9,d10,dj,dq,dk,da,d2,h3,h4,h5,h6,h7,h8,h9,h10,hj,hq,hk,ha,h2]
+
+let userDisEl0 = getById('userDis0')
+let userDisEl1 = getById('userDis1')
+let userDisEl2 = getById('userDis2')
+let userDisEl3 = getById('userDis3')
+let userDisEl4 = getById('userDis4')
+let userDisEl5 = getById('userDis5')
+let userDisEl6 = getById('userDis6')
+let userDisEl7 = getById('userDis7')
+let userDisEl8 = getById('userDis8')
+let userDisEl9 = getById('userDis9')
+let userDisEl10 = getById('userDis10')
+let userDisEl11 = getById('userDis11')
+let userDisEl12 = getById('userDis12')
+
+let usersHandDisplaySpots = [userDisEl0,userDisEl1,userDisEl2,userDisEl3,userDisEl4,userDisEl5,userDisEl6,userDisEl7,userDisEl8,userDisEl9,userDisEl10,userDisEl11,userDisEl12]
+
+displayHand(pickHand(deal(deck),0))
+
+
